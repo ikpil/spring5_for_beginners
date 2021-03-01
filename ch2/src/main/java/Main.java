375854877f99d7cf4213606d21d4247abcd10a73
ikpil <ikpil0@gmail.com>
2021-03-02 00:47:16 +0900

    챕터 2에서 어떻게 Bean 객체를 관리하는지 보여준다.

--- ch2/src/main/java/Main.java
+++ ch2/src/main/java/Main.java
@@ -8,8 +8,13 @@ public class Main {
         var ctx = new AnnotationConfigApplicationContext(AppContext.class);
 
         // getBean 은 메서드 이름과 타입을 읽어 greeter() 가 생성한 Greeter 객체를 반환한다.
-        var g = ctx.getBean("greeter", Greeter.class);
-        var msg = g.greet("스프링");
+        var g1 = ctx.getBean("greeter", Greeter.class);
+        var g2 = ctx.getBean("greeter", Greeter.class);
+
+        // g1, g2 를 생성하고 비교했을 때, 같다. 이로써 기본 설정이 싱글턴 이다.
+        System.out.println("(g1 == g2) = " + (g1 == g2));
+
+        var msg = g1.greet("스프링");
         System.out.println(msg);
         ctx.close();
     }
